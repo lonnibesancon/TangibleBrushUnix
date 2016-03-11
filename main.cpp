17cec15784295346623dcbe59d238c7e9814fdc0
testmachineubuntu <lonni.besancon@gmail.com>
2016-03-11 18:33:34 +0100

    last version

--- main.cpp
+++ main.cpp
@@ -138,7 +138,8 @@ int main()
 			}
 		}
 		dataMatrix = server.getDataMatrix();
-		sliceMatrix= server.getSliceMatrix();
+		sliceMatrix = server.getSliceMatrix();
+		//sliceMatrix = dataMatrix * sliceMatrix ;
 		seedPoint = server.getSeedPoint();
 		app->setMatrices(dataMatrix,sliceMatrix);
 		/*app->setMatrices(Matrix4::makeTransform(Vector3(0, 0, 380), Quaternion(Vector3::unitX(), -M_PI/4)*Quaternion(Vector3::unitZ(), t)),
