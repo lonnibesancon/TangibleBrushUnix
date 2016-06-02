ae2092597292fb8d099467d7cd5999beb631dab3
Gaulois94 <serenomickael@gmail.com>
2016-06-02 00:18:32 +0000

    Basique rectangle shape

---
+++ rendering/rectangle.cpp
@@ -0,0 +1,73 @@
+#include "rendering/rectangle.h"
+#include "material.h"
+
+namespace
+{
+	GLfloat vertices[] = {1.0f, 1.0f, 0.0f,  0.0f, 1.0f, 0.0f,  0.0f,0.0f, 0.0f,      // v0-v1-v2 (front)
+						   0.0f,0.0f, 0.0f,   1.0f,-0.0f, 0.0f,   1.0f, 1.0f, 0.0f};
+
+	const char* vertexShader =
+		"#version 100\n"
+		"uniform highp mat4 projection;\n"
+		"uniform highp mat4 modelView;\n"
+		"attribute highp vec3 vertex;\n"
+		"void main() {\n"
+		"  gl_Position = projection * modelView * vec4(vertex, 1.0);\n"
+		"}";
+
+	const char* fragmentShader =
+		"#version 100\n"
+		"uniform lowp vec4 color;\n"
+		"void main() {\n"
+		"  gl_FragColor = color;\n"
+		"}";
+} //namespace
+
+Rectangle::Rectangle(float width, float height) : m_width(width), m_height(height), m_material(new Material(vertexShader, fragmentShader)),
+	m_bound(false), m_vertexAttrib(-1), m_projectionUniform(-1), m_modelViewUniform(-1), m_colorUniform(-1),
+	m_color(Vector3(0.0, 0.0, 1.0)), m_opacity(1.0f)
+{}
+
+void Rectangle::bind()
+{
+	m_material->bind();
+
+	m_vertexAttrib = m_material->getAttribute("vertex");
+	m_modelViewUniform = m_material->getUniform("modelView");
+	m_projectionUniform = m_material->getUniform("projection");
+	m_colorUniform = m_material->getUniform("color");
+
+	android_assert(m_vertexAttrib != -1);
+	android_assert(m_modelViewUniform != -1);
+	android_assert(m_projectionUniform != -1);
+	android_assert(m_colorUniform != -1);
+
+	m_bound = true;
+}
+
+void Rectangle::render(const Matrix4& projectionMatrix, const Matrix4& modelViewMatrix)
+{
+	if (!m_bound)
+		bind();
+
+	glUseProgram(m_material->getHandle());
+
+	// Vertices
+	glVertexAttribPointer(m_vertexAttrib, 3, GL_FLOAT, false, 0, vertices);
+	glEnableVertexAttribArray(m_vertexAttrib);
+
+	// Uniforms
+	glUniformMatrix4fv(m_projectionUniform, 1, false, projectionMatrix.data_);
+	glUniformMatrix4fv(m_modelViewUniform, 1, false, (modelViewMatrix * Matrix4(Matrix4::identity()).rescale(Vector3(m_width, m_height, 1.0))).data_);
+	glUniform4f(m_colorUniform, m_color.x, m_color.y, m_color.z, 0.0f);
+
+	// Rendering
+	glDrawArrays(GL_TRIANGLES, 0, 6);
+	glDisableVertexAttribArray(m_vertexAttrib);
+}
+
+void Rectangle::setSize(float w, float h)
+{
+	m_width = w;
+	m_height = h;
+}
---
+++ rendering/rectangle.h
@@ -0,0 +1,25 @@
+#ifndef  RECTANGLE_INC
+#define  RECTANGLE_INC
+
+#include "global.h"
+#include "renderable.h"
+
+class Rectangle : public Renderable
+{
+	public:
+		Rectangle(float width, float height);
+		void bind();
+		void render(const Matrix4& projMat, const Matrix4& mvp);
+		void setSize(float w, float h);
+
+	private:
+		float m_width, m_height;
+		MaterialSharedPtr m_material;
+		bool m_bound;
+		GLint m_vertexAttrib;
+		GLint m_projectionUniform, m_modelViewUniform, m_colorUniform;
+		Vector3 m_color;
+		float m_opacity;
+};
+
+#endif
